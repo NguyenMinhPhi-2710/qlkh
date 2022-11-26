596bc87285a43c49bb32b553d1726c2fa2b02510
NguyenMinhPhi-2710 <114756113+NguyenMinhPhi-2710@users.noreply.github.com>
2022-11-26 23:06:25 +0700

    update main nhe

---
+++ src/qlkh/StaffList.java
@@ -0,0 +1,210 @@
+package qlkh;
+import java.util.Arrays;
+import java.io.*;
+public class StaffList {
+    private static Staff[] list;
+
+    public StaffList(){
+        list=null;
+    }
+    public StaffList(Staff[] list){
+        this.list=list;
+    }
+
+    // create list and read data from file
+    public void createList(File staffFile,File addressFile){  
+        list = new Staff[0];
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(addressFile));
+            String line = br.readLine();
+            Address[] addressArray= new Address[0];
+            while(line != null){
+                String[] row = line.split(",");
+                Address address = new Address(Integer.parseInt(row[0]), row[1], row[2], row[3], row[4]);
+                addressArray=addAddressFromFile(address, addressArray);
+                line = br.readLine();
+            }
+            br.close();
+
+            br = new BufferedReader(new FileReader(staffFile));
+            line = br.readLine();
+            int index=0;
+            while(line != null){
+                String[] row = line.split(",");
+                Staff staff = new Staff(row[0], row[1], addressArray[index], row[2], row[3]);
+                list = addStaffFromFile(staff, list);
+                index++;
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot read the file, using alternative solution instead.");
+
+            Address address = new Address(1,"Duong 1", "P1", "Q1", "TP Mong Mo");
+            Staff staff = new Staff("staff1", "Nguyen Van A", address, "0123", "Quan ly");
+            list=addStaffFromFile(staff, list);
+
+            address = new Address(2,"Duong 2", "P2", "Q2", "TP Mong Mo");
+            staff = new Staff("staff2", "Nguyen Van B", address, "0234", "Nhan vien");
+            list=addStaffFromFile(staff, list);
+
+            address = new Address(3,"Duong 3", "P3", "Q3", "TP Mong Mo");
+            staff = new Staff("staff3", "Nguyen Van C", address, "0345", "Nhan vien");
+            list=addStaffFromFile(staff, list);
+            
+            address = new Address(4,"Duong 4", "P4", "Q4", "TP Mong Mo");
+            staff = new Staff("staff4", "Nguyen Van D", address, "0456", "Nhan vien");
+            list=addStaffFromFile(staff, list);
+
+            address = new Address(5,"Duong 5", "P5", "Q5", "TP Mong Mo");
+            staff = new Staff("staff5", "Nguyen Van E", address, "0567", "Nhan vien");
+            list=addStaffFromFile(staff, list);
+
+            address = new Address(6,"Duong 6", "P6", "Q6", "TP Mong Mo");
+            staff = new Staff("staff6", "Nguyen Van F", address, "0678", "Nhan vien");
+            list=addStaffFromFile(staff, list);
+
+            address = new Address(7,"Duong 7", "P7", "Q7", "TP Mong Mo");
+            staff = new Staff("staff7", "Nguyen Van G", address, "0789", "Nhan vien");
+            list=addStaffFromFile(staff, list);
+
+            address = new Address(8,"Duong 8", "P8", "Q8", "TP Mong Mo");
+            staff = new Staff("staff8", "Nguyen Van H", address, "0890", "Nhan vien");
+            list=addStaffFromFile(staff, list);
+
+            address = new Address(9,"Duong 9", "P9", "Q9", "TP Mong Mo");
+            staff = new Staff("staff9", "Nguyen Van I", address, "0901", "Nhan vien");
+            list=addStaffFromFile(staff, list);
+
+            address = new Address(10,"Duong 10", "P10", "Q10", "TP Mong Mo");
+            staff = new Staff("staff10", "Nguyen Van J", address, "0902", "Nhan vien");
+            list=addStaffFromFile(staff, list);
+
+            updataFile(staffFile, addressFile);
+        }
+        finally{
+            try {
+                br.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // read address date from file
+    public static Address[] addAddressFromFile(Address address,Address[] arr){
+        int n = arr.length;
+        arr = Arrays.copyOf(arr, n+1);
+        arr[n] = address;
+        return arr;
+    }
+
+    //read staff data from file
+    public static Staff[] addStaffFromFile(Staff staff,Staff[] arr){
+        int n = arr.length;
+        arr = Arrays.copyOf(arr, n+1);
+        arr[n] = staff;
+        return arr;
+    }
+
+    // add staff to list
+    public void addStaff(Staff staff){
+        list = addStaffFromFile(staff, list);
+        System.out.println("Nhan vien da duoc them thanh cong!!");
+
+    }
+
+    // remove staff from list
+    //                               staffName: name of the staff that need to be deleted
+    public void removeStaff(String staffName){
+
+        if (list==null)
+            System.out.println("Khong ton tai danh sach");
+        else{
+            Staff[] proxyList = new Staff[list.length-1];
+            for (int i = 0, k = 0; i < list.length; i++){
+                if (list[i].getStaffName().equals(staffName))
+                    // ignore the staff that need to be deleted
+                    continue;
+                else
+                    try {
+                        proxyList[k++]=list[i];
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Khong tim thay nhan vien.");
+                    }
+            }
+            list = proxyList;
+        }
+    }
+
+    // override new data to file
+    // MUST give this warning: "You MUST choose "Submit change & exit"(press _) to save all your changes"
+    public static void updataFile(File staffFile, File addressFile){
+        BufferedWriter bw1 = null;
+        BufferedWriter bw2 = null;
+        try {
+            bw1 = new BufferedWriter(new FileWriter(staffFile,false));
+            bw2 = new BufferedWriter(new FileWriter(addressFile,false));
+            for(Staff staff : list){
+                bw1.write(staff.getStaffID() +",");
+                bw1.write(staff.getStaffName() +",");
+                bw1.write(staff.getPhoneNumber() +",");
+                bw1.write(staff.getPosition() +"\n");
+                bw2.write(staff.getAddress() +"\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot update data!!");
+        } finally{
+            try {
+                bw1.close();
+                bw2.close();
+            } catch (Exception e) {
+                System.out.println("Cannot close the stream!!");
+            }
+        }
+    }
+
+    // change staff information
+    public void adjustStaff(String staffID){
+        for (Staff staff : list){
+            if(staff.getStaffID().equals(staffID)){
+                staff.input();
+            }
+            else
+                System.out.println("Khong the tim thay nhan vien.");
+        }
+    }
+
+    // find staff
+    public void findStaff(String staffID){
+        for (Staff staff : list){
+            if(staff.getStaffID().equals(staffID)){
+                System.out.println("Nhan vien tim thay!!");
+                System.out.println(staff);
+            }
+            else
+                System.out.println("Khong the tim thay nhan vien.");
+        }
+    }
+
+    // check staff whether it exists or not
+    public boolean checkStaff(String staffID){
+        for (Staff staff : list){
+            if(staff.getStaffID().equals(staffID))
+                return true;
+        }
+        return false;
+        
+    }
+
+    // get the amount of staff 
+    public void getAmount(){
+        System.out.println("Staff number: "+list.length);
+    }
+
+    public void displayList(){
+        for(Staff staff : list)
+            System.out.println(staff);
+    }
+}
---
+++ src/qlkh/Tools.java
@@ -0,0 +1,222 @@
+package qlkh;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+/**
+ *
+ * @author Lilly, Ken
+ */
+public class Tools {
+// --Properties-----------------------------------------------------------------
+    // --Console scan--
+
+    public static Scanner scan = new Scanner(System.in);
+
+// --Functions check------------------------------------------------------------    
+    // --StaffID--
+    public static boolean isStaffID(String staffID) {
+        if (staffID.length() != 7) {
+            return false;
+        }
+
+        if (!staffID.startsWith("staff")) {
+            return false;
+        }
+
+        String temp = staffID.substring(5);
+        if (!isLong(temp) || temp.equalsIgnoreCase("00")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // --SupplierID--
+    public static boolean isSupplierID(String supplierID) {
+        if (supplierID.length() != 3) {
+            return false;
+        }
+
+        if (supplierID.charAt(0) != 'S') {
+            return false;
+        }
+
+        String temp = supplierID.substring(1);
+        if (!isLong(temp) || temp.equalsIgnoreCase("00")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // --DistributorID--
+    public static boolean isDistributorID(String distributorID) {
+        if (distributorID.length() != 3) {
+            return false;
+        }
+
+        if (distributorID.charAt(0) != 'D') {
+            return false;
+        }
+
+        String temp = distributorID.substring(1);
+        if (!isLong(temp) || temp.equalsIgnoreCase("00")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // --PositionID--X00--
+    public static boolean isPositionID(String positionID) {
+        if (positionID.length() != 3) {
+            return false;
+        }
+
+        char area = positionID.charAt(0);
+        char shelf1 = positionID.charAt(1);
+        char shelf2 = positionID.charAt(2);
+        if (!(('A' <= area && area <= 'Z')
+                && ('0' <= shelf1 && shelf1 <= '9')
+                && ('0' <= shelf2 && shelf2 <= '9'))
+                || (shelf1 == 0 && shelf2 == 0)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // --ProductID--
+    public static String isProductID() {
+        String productId;
+        System.out.println("Nhap id (VD: P01): ");
+        do {
+            productId = scan.nextLine();
+            productId = productId.toUpperCase();
+            productId = productId.trim();
+            String numId = productId.substring(1);
+            if (!productId.startsWith("P") || productId.endsWith("00") || !isInteger(numId) || productId.length() != 3
+                    || productId == null) {
+                System.out.println("Sai dinh dang! Nhap lai id (VD: P01): ");
+            } else {
+                break;
+            }
+        } while (true);
+        return productId;
+    }
+
+    public static boolean isProductID(String productID) {
+        if (productID.length() != 3) {
+            return false;
+        }
+
+        if (productID.charAt(0) != 'P') {
+            return false;
+        }
+
+        String temp = productID.substring(1);
+        if (!isLong(temp) || temp.equalsIgnoreCase("00")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // --InvoiceID--X00--
+    public static boolean isInvoiceID(String invoiceID) {
+        if (invoiceID.length() != 3) {
+            return false;
+        }
+
+        if (invoiceID.charAt(0) != 'I' && invoiceID.charAt(0) != 'E') {
+            return false;
+        }
+
+        String temp = invoiceID.substring(1);
+        if (!isLong(temp) || temp.equalsIgnoreCase("00")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // --Unit--Cai-Chiec--
+    public static boolean isUnit(String unit) {
+
+        return true;
+    }
+
+    // --Date format--dd/MM/yyyy--
+    public static boolean isDate(String date) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Calendar element = Calendar.getInstance();
+            element.setTime(new SimpleDateFormat("dd/MM/yyyy").parse(date));
+            return true;
+        } catch (ParseException ex) {
+            return false;
+        }
+    }
+
+    // --Interger check--
+    public static boolean isInteger(String element) {
+        try {
+            Integer.parseInt(element);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // --Long check--
+    public static boolean isLong(String element) {
+        try {
+            Long.parseLong(element);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // --Double check--
+    public static boolean isDouble(String element) {
+        try {
+            Double.parseDouble(element);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // --Boolean check--
+    public static boolean isBoolean(String element) {
+        try {
+            Boolean.parseBoolean(element);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+// --Methods--------------------------------------------------------------------
+    // --Press any key to continue--
+    public static void continute() {
+        System.out.println("\n---Nhan Enter de tiep tuc---");
+        Scanner scan = new Scanner(System.in);
+        String continute = scan.nextLine();
+    }
+
+    // --Clear sceen console--
+    public static void cls() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (IOException | InterruptedException ex) {
+        }
+    }
+}
